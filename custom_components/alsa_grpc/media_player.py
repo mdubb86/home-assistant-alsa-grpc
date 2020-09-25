b0481f2afd6677dfd9c4ec93b35bac5420262e1c
Michael Waage <mwaage@supplyframe.com>
2020-09-25 11:29:46 -0500

    add unique ID including prefix of server

--- custom_components/alsa_grpc/media_player.py
+++ custom_components/alsa_grpc/media_player.py
@@ -21,7 +21,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Required(CONF_SERVERS): [{
             vol.Required('ip'): cv.string,
             vol.Optional('port'): cv.port,
-            vol.Required("alias"): cv.string
+            vol.Required('prefix'): cv.string
         }]
     }
 )
@@ -44,33 +44,34 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     for server in servers:
         ip = server['ip']
         port = server['port']
-        alias = server['alias']
+        prefix = server['prefix']
 
-        def _on_connect(ctrl, alias=alias):
-            _LOGGER.info("Configuring alsa-grpc entity %s %s", alias, ctrl.name)
-            add_entities([AlsamixerClientEntity(alias, ctrl)])
+        def _on_connect(ctrl, prefix=prefix):
+            add_entities([AlsaGrpcControlEntity(prefix, ctrl)])
 
         client = AlsaClient(ip, port, _on_connect)
         clients.append(client)
-        _LOGGER.info("Connecting to alsa-grpc server %s:%d (%s)", ip, port, alias)
+        _LOGGER.info("Connecting to alsa-grpc server %s:%d (%s)", ip, port, prefix)
         client.connect()
 
 
-class AlsamixerClientEntity(MediaPlayerEntity):
+class AlsaGrpcControlEntity(MediaPlayerEntity):
     """Representation of an alsa-grpc control."""
 
-    def __init__(self, alias, ctrl):
+    def __init__(self, prefix, ctrl):
         """Initialize the alsa-grpc control entity."""
         self._ctrl = ctrl
-        self._name = alias + " " + ctrl.name
-        self._volume = ctrl.volume[0] / 100
+        self.uid = f'alsa_{prefix}_{ctrl.name.lower()}'
+        self.uid = f'{prefix}_{ctrl.name.lower()}_alsa_control'
         self._state = STATE_IDLE
+        self._volume = ctrl.volume[0] / 100
 
         def _callback():
             self._volume = ctrl.volume[0] / 100
             self.schedule_update_ha_state()
 
         ctrl.subscribe(_callback)
+        _LOGGER.info("Configured alsa-grpc entity %s", self._uid)
 
     def set_volume_level(self, volume):
         """Set the volume level."""
@@ -79,6 +80,11 @@ class AlsamixerClientEntity(MediaPlayerEntity):
     @property
     def should_poll(self):
         return False
+
+    @property
+    def unique_id(self):
+        """Return the unique ID of the control"""
+        return self._uid
 
     @property
     def name(self):
